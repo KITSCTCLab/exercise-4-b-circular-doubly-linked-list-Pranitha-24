96f6079cbb4b8c08000ef739573cf8a583723563
Pranitha-24 <109952550+Pranitha-24@users.noreply.github.com>
2022-08-12 21:29:31 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,16 @@
 # Read an integer that denotes the length of the list which is returned as the output of the algorithm
 length_of_circular_linked_list = int(input())
-# Read space-separated integers that denote the elements of the listT which is returned as the output of the algorithm
+# Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
 circular_linked_list = list(map(int,input().strip().split(" ")))
-
 # Write your code here
 actual_list = []
-index = 0
-while len(actual_list) < length_of_circular_linked_list and index < len(circular_linked_list):
-    element = circular_linked_list[index]
+
+value = 0
+while len(actual_list) < length_of_circular_linked_list and value < len(circular_linked_list):
+    element = circular_linked_list[value]
     if element not in actual_list:
         actual_list.append(element)
-    index += 1
+    value += 1
 
-print(" ".join(str(num) for num in actual_list))
+print(len(actual_list))
+print(" ".join(str(num) for num in actual_list)
